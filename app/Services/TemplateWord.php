ed26a62b3023ce20ed24f00407a6b35f5fedf614
Journey PC white <ginopaflo001608@gmail.com>
2024-11-11 23:34:12 -0500

    feat: template docs

--- app/Services/TemplateWord.php
+++ app/Services/TemplateWord.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
 
 class TemplateWord
 {
@@ -11,8 +12,26 @@ class TemplateWord
         public string $subject, 
         public string $officeOneAboveAll, 
         public string $typeDoc,
-        public string $office = null
+        public ?string $office = null
     ){
+        $this->metadata = Cache::rememberForever('metadata_template', function(){
+            return DB::table('metada_template')
+                ->select(
+                    'year_name AS yearName',
+                    'path_logo AS pathLogo',
+                    'address_footer AS addressFooter',
+                    'margin_top AS marginTop',
+                    'margin_bottom AS marginBottom',
+                    'margin_left AS marginLeft',
+                    'margin_right AS marginRight',
+                )
+                ->first();
+        });
+    }
 
+    public function run(): string
+    {
+        
+        return '';
     }
 }
