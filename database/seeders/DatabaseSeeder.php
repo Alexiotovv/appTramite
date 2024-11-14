c64a5b9ed98525811ecfa07acf807285904810bc
journii gorle <ginopaflo001608@gmail.com>
2024-11-14 12:42:02 -0500

    refactor: :art:

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,47 +5,19 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use DB;
-use Illuminate\Support\Facades\Hash;
+use Database\Seeders\Fakers\Offices;
+use Database\Seeders\Fakers\Faker;
+use Database\Seeders\RolePermissions;
 
 class DatabaseSeeder extends Seeder
 {
 
     public function run(): void
     {
-
-        DB::table('users')->insert([
-            [
-            'name'=>'admin',
-            'email' => 'admin@gmail.com',
-            'role' => 'admin',
-            'status' => 1,
-            'password' => Hash::make('123456'),
-            'status'=>1],
-            [
-                'name'=>'test',
-                'email' => 'test@gmail.com',
-                'role' => 'asistente',
-                'status' => 1,
-                'password' => Hash::make('123456'),
-                'status'=>1,
-            ]
+        $this->call([
+            Offices::class,
+            RolePermissions::class,
+            Faker::class,
         ]);
-        
-        DB::table('niveles')->insert([
-            [
-                'nombre'=>'Nivel 1',
-                'descripcion'=>'Máximo Nivel',
-            ],
-            [
-                'nombre'=>'Nivel 2',
-                'descripcion'=>'Primer SubNivel',
-            ],
-            [
-                'nombre'=>'Nivel 3',
-                'descripcion'=>'Tercer Nivel',
-            ]
-        ]);
-        
     }
 }
