e42f3761736943e33be417720d791d69f7de642d
journii gorle <ginopaflo001608@gmail.com>
2024-12-27 13:43:17 -0500

    fix: commit

--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -12,8 +12,9 @@ class TemplateController extends Controller
     public function list(Lister $request): JsonResponse
     {
         try{
+
             
-            return response()->json();
+            return response()->json(['items' => , 'total_items' => ], 200);
         }catch(Exception $e){
             $this->LogError(get_class($this), $e, __FUNCTION__);
             return $this->defaultResponse($e);
